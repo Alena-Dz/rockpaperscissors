0e9c57069838f2a5967a892cfc86db5dc428dab5
Lena <alenadzubic@protonmail.com>
2022-10-25 23:16:01 -0500

    added function to play 1 round of game

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"];
+    let choices = ["rock", "paper", "scissors"];
     let randomChoice = Math.floor(Math.random() * choices.length);
     console.log(randomChoice);
     let computerChoice = choices[randomChoice];
     console.log(computerChoice)
+    return computerChoice;
 }
 
-getComputerChoice();
+function playerVsComputer (playerSelection, computerSelection) {
+    computerSelection = getComputerChoice();
+    playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
+    console.log(computerSelection);
+    console.log(playerSelection);
+    if (computerSelection == playerSelection){
+        document.write("It's a draw!");
+    } else if (playerSelection == "paper" && computerSelection == "rock" || playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "paper") {
+        document.write("Yay, you won!");
+    } else {
+        document.write("Sorry, you lose :(");
+    }
+}
+
+playerVsComputer();
